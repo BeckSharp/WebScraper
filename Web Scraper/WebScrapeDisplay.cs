3af9b7d3bf67feedb57fd5439296cbc285bb44ef
BeckSharp <tombeck34610@yahoo.com>
2019-12-23 20:53:40 +0000

    Bug found in last commit has now been fixed so no html code is displayed anymore. All code is commented and has been tested.

--- Web Scraper/WebScrapeDisplay.cs
+++ Web Scraper/WebScrapeDisplay.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Threading; //Required for CancellationTokenSource.
-using AngleSharp.Html.Dom; //Required to use IHtmlDocument.
-using AngleSharp.Html.Parser; //Required for HtmlParser.
-using System.Net.Http; // Required for HTPP objects.
-using System.IO; //Required to use Stream.
+using System.Threading;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System.Net.Http;
+using System.IO; 
 using AngleSharp.Dom;
 using AngleSharp.Text;
-using System.Windows;
-
+/* Package reference uses:
+ * System.Threading - Required for CancellationTokenSource.
+ * AngleSharp.Html.Dom - Required to use IHtmlDocument.
+ * AngleSharp.Html.Parser - Required for HtmlParser.
+ * System.Net.Http - Required for HTPP objects.
+ * System.IO - Required to use Stream.
+ */
 
 namespace Web_Scraper
 {
@@ -83,28 +83,35 @@ namespace Web_Scraper
             }            
         }
 
-        //Cleaning up results and appending to rich text box.
+        //Cleaning up every results so that it can be appended  to the rich text box.
         public void PrintResults(IEnumerable<IElement> articleLink)
         {
             foreach (var element in articleLink)
             {
-                CleanResults(element);
-
-                richTextBox.AppendText($"{Title} - {Url}{Environment.NewLine}");
+                CleanResults(element);             
             }
         }
 
-        //Replacing html code with place markers, and makes text look presentable.
+        //Replacing html code with place markers, and making text look presentable for a display.
         private void CleanResults(IElement result)
         {
-            //Retrieving link from html code.
+            //Retrieving URL link from html code.
             string htmlResult = result.InnerHtml.ReplaceFirst("        <span class=\"field-content\"><div><a href=\"", "https://www.oceannetworks.ca");
             htmlResult = htmlResult.ReplaceFirst("\">", "*");
             //Retrieving title from html code.
             htmlResult = htmlResult.ReplaceFirst("</a></div>\n<div class=\"article-title-top\">", "-");
             htmlResult = htmlResult.ReplaceFirst("</div>\n<hr></span>  ", "");
 
-            SplitResults(htmlResult);
+            ResultValidation(htmlResult);
+        }
+
+        //Validating that no invalid html code will be passed through the code to be displayed to the user after results have been cleaned.
+        private void ResultValidation(string htmlResult)
+        {
+            if ((htmlResult.Contains("<") || htmlResult.Contains(">")) == false)
+            {
+                SplitResults(htmlResult);
+            }
         }
 
         //Splitting htmlResult into 2 pieces of data using the '*' placeholdedr from CleanResults().
@@ -113,7 +120,13 @@ namespace Web_Scraper
             string[] splitResults = htmlResult.Split('*');
             Url = splitResults[0];
             Title = splitResults[1];
+            AppendResults();
         }
 
+        //Appending result to the rich text box.
+        private void AppendResults()
+        {
+            richTextBox.AppendText($"{Title} - {Url}{Environment.NewLine}");
+        }
     }
 }
